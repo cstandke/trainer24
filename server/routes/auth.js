425f474793ae4e814bb21d512abaf953e35d9f9f
annearbor <betting.anne@gmail.com>
2019-03-12 22:22:05 +0100

    checks if user already exists on singup, adds some links from signup/login and viceversa

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,36 +5,31 @@ const router = express.Router();
 const User = require("../models/basic-user");
 
 // POST route => to create a new user-type trainer
+// check if user email or username already exists, if not then create new user
 
 router.post("/signup-trainer", (req, res, next) => {
-  //  User.findOne({ email = user.email }).then(user => {
-
-  // if (user === null) {
-
-  //get call first?
-  // router.get('/projects', (req, res, next) => {
-  //     Project.find().populate('tasks')
-  //       .then(allTheProjects => {
-  //         res.json(allTheProjects);
-  //       })
-  //       .catch(err => {
-  //         res.json(err);
-  //       })
-  //   });
-
-  User.create({
-    type: "trainer",
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    username: req.body.username,
-    email: req.body.email
-  })
-    .then(response => {
-      res.json(response);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+  // User.findOne({ username: req.body.username }).then(user => {
+  User.findOne({
+    $or: [{ username: req.body.username }, { email: req.body.email }]
+  }).then(user => {
+    if (user === null) {
+      User.create({
+        type: "trainer",
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email
+      })
+        .then(response => {
+          res.json(response);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    } else {
+      res.send("The user already exists");
+    }
+  });
 });
 
 // POST route => to create a new user-type trainer
@@ -52,6 +47,18 @@ router.post("/signup-learner", (req, res, next) => {
   })
     .then(response => {
       res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// GET route => to get all the user
+router.get("/users", (req, res, next) => {
+  Project.find()
+    .populate("tasks")
+    .then(allTheProjects => {
+      res.json(allTheProjects);
     })
     .catch(err => {
       res.json(err);
