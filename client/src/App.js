f148da7cbfbc80e07dbf3d551e9560d59ace01b3
annearbor <betting.anne@gmail.com>
2019-03-12 16:51:08 +0100

    adds user model and BE auth routes (signup-trainer and signup-learner for API post call, adds axios (client) and login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from "react";
+
+import Homepage from "../src/components/Homepage";
+import Navbar from "../src/components/Navbar";
 // import logo from "./logo.svg";
-// import { Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Signup from "./components/auth/Signup";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
+        <Homepage />
         <Signup />
-        {/* <Switch> */}
-        {/* <Route
+        <Switch>
+          {/* <Route
             exact
             path="/signup"
             // render={() => <Signup getUser={this.getTheUser} />}
           /> */}
-        {/* <Route exact path="/projects" component={Signup} /> */}
-        {/* </Switch> */}
+          <Route exact path="/login" component={Login} />
+        </Switch>
       </div>
     );
   }
