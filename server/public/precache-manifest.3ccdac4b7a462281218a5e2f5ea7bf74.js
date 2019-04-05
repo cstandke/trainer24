142210f04219f5d45d8f57c0f140ea7fe02c4481
Christian Standke <c.standke@gmail.com>
2019-04-05 23:42:28 +0200

    configured server

---
+++ server/public/precache-manifest.3ccdac4b7a462281218a5e2f5ea7bf74.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a23fc3de59c1aed501e3411b7ceeb07",
+    "url": "/static/media/mobile_1.5a23fc3d.png"
+  },
+  {
+    "revision": "b11539aa6b2a1b523e5faf8257ec73ee",
+    "url": "/static/media/hero_small.b11539aa.png"
+  },
+  {
+    "revision": "826db2e9f37d71c829da9474c759dad2",
+    "url": "/static/media/hero.826db2e9.png"
+  },
+  {
+    "revision": "8c3a5c9a4f52ca39695a8226c053ba6c",
+    "url": "/static/media/course.8c3a5c9a.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ac53711554e186cc29e6",
+    "url": "/static/js/main.e7c73a5c.chunk.js"
+  },
+  {
+    "revision": "42ebdf8f6ba3e730f5f8",
+    "url": "/static/js/2.be6fd9b1.chunk.js"
+  },
+  {
+    "revision": "ac53711554e186cc29e6",
+    "url": "/static/css/main.1e8fcf74.chunk.css"
+  },
+  {
+    "revision": "3330504adeb6c4c027be0ce89f1596ae",
+    "url": "/index.html"
+  }
+];
